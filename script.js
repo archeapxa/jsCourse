098f46790577ead098d239d010f7ffc296b1a4bf
archeapxa <stanislav.iushkevich@gmail.com>
2021-09-01 13:54:50 +0300

    lesson01 done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-var myVar = 10;
+let title, screens, screenPrice, rollback, fullPrice, adaptive;
+
+title = "Title";
+screens = 2;
+screenPrice = 190;
+rollback = true;
+fullPrice = 200;
+adaptive = true;
+
+alert("My first Alert!");
+console.log("My first log!");
